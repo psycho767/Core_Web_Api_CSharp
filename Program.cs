44dceb99e16316804c43397208ad78f0cecd527a
Psycho <hiteshgautam767@gmail.com>
2024-08-08 13:58:02 +0530

    Student Model Added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
+using API_WebApp;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+//List<StudentModel> student = new List<StudentModel>
+//{
+//    new StudentModel{id:1,Name:"Hitesh",Age:20,email:"hitesh@gmail.com",address:"noida" }
+//}
+
+List<StudentModel> students = new List<StudentModel>
+{
+     new StudentModel { Id = 1, Name = "Hitesh", Age = 30, Email = "hitesh@gmail.com", Address = "Noida" },
+    new StudentModel { Id = 2, Name = "Gaurav", Age = 40, Email = "gaurav@gmail.com", Address = "Gr.Noida" },
+     new StudentModel { Id = 3, Name = "Saurabh", Age = 30, Email = "saurabh@gmail.com", Address = "Bulandsher" },
+     new StudentModel { Id = 3, Name = "Rahul", Age = 25, Email = "rahul@gmail.com", Address = "Bihar" }
+   
+};
+
+
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ StudentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace API_WebApp
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
